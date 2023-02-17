3acdb3484e49b7438a1cb4af5c5b8d010746bb3c
VladimirSeldev <vseldev@gmail.com>
2023-02-17 20:14:02 +0300

    Task0

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача№0
+// Напишите команду, которая на вход принимает число и
+// выдает ее квадрат
+Console.WriteLine("Введите число: ");
+// Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+
+// Проверяем, чтобы данные были не пустыми
+if (inputNum != null)
+{
+    // Парсим введенное число
+    int number = int.Parse(inputNum);
+    // Находим квадрат числа
+    int outNum = number * number;
+    // Выводим данные в консоль
+    Console.WriteLine("Квадрат числа: "+outNum);
+
+}
