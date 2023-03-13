ba0ad318373ced4e1af706aa59fa5cec2e507ed8
VladimirSeldev <vseldev@gmail.com>
2023-03-13 21:49:59 +0300

    Task13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №13 
+// Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем. 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция вывода цифры стоящей в середине трехзначногно числа.
+int InCenter(int a)
+{
+    
+    int result = ((a / 100) % 100);
+    return result;
+}
