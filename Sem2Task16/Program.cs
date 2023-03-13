c1a20a89799049363a4918ffc20954e53bfffe77
VladimirSeldev <vseldev@gmail.com>
2023-03-13 22:22:55 +0300

    Task16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число
+// квадратом другого. 
+void squareTest(int fistNum, int secondNum)
+{
+    if (fistNum == Math.Pow(secondNum, 2))
+    {
+        Console.WriteLine($"Число {fistNum} является квадратом {secondNum}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {fistNum} не является квадратом {secondNum}");
+    }
+}
+
+Console.WriteLine("Введите первое число: ");
+int fistNumer = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumer = Convert.ToInt32(Console.ReadLine());
+
+squareTest(fistNumer,secondNumer);
+
+squareTest(secondNumer,fistNumer);
