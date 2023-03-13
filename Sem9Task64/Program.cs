fe06fbebd356c2074299f65fae1be4b5ae2411ab
VladimirSeldev <vseldev@gmail.com>
2023-03-13 23:55:36 +0300

    Task64

--- Sem9Task64/Program.cs
+++ Sem9Task64/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №64 
+// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от 1 до N. 
+// Выполнить с помощью рекурсии.
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void che(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m}, ");
+}
+che(m+1,n);
+
+}
+che(m,n);
