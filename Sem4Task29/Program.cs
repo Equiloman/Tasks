25bc183768beb40c3817dd3039a9c7316ad9207c
VladimirSeldev <vseldev@gmail.com>
2023-03-13 22:45:55 +0300

    Task29

---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №29
+// Напишите программу, которая задаёт массив из 
+// N элементов и выводит их на экран. Ввести с клавиатуры 
+// длину массива и диапазон значений элементов.
+static void Main()
+{
+    int size = 20;
+    int[] array = new int[20];
+    Random myRandom = new Random();
+
+    Console.WriteLine("Вывод с помощью for");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = myRandom.Next(0, 20);
+        Console.Write("{0} ", array[i]);
+    }
+
+    Console.WriteLine("\n\nВывод с помощью foreach");
+    foreach (var elem in array)
+    {
+        Console.Write("{0} ", elem);
+    }
+}
