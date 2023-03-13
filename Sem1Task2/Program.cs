e930ab6ac54172302e8420a2b71365c1d1d84af2
VladimirSeldev <vseldev@gmail.com>
2023-03-13 21:01:21 +0300

    Task2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №2
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+int a = new Random().Next(0, 1000); 
+int b = new Random().Next(0, 1000); 
+Console.Write("a=");
+Console.Write(a);
+if (a > b)
+{
+    Console.Write(" больше ");
+}
+else if (a == b)
+{
+    Console.Write(" равен ");
+}
+else
+{
+    Console.Write(" меньше ");
+};
+Console.Write("b=");
+Console.WriteLine(b);
